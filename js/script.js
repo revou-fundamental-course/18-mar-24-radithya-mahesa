841ed56824dba2ecca7917b2db94e516a48c15c9
radithya-mahesa <radithya.syabil.com>
2024-03-27 00:52:22 +0700

    modified html css js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-window.onload = function replaceName(){
-    let name = prompt("May I know Your name?", "User")
-    document.getElementById("name").innerHTML = name
+const replaceName = function() {
+    let name = prompt("May I know Your name?", "User");
+    document.getElementById("name").innerHTML = name;
+};
+window.onload = replaceName;
+
+function validateForm() {
+    var name = document.getElementsByClassName('name-input')[0].value;
+    var email = document.getElementsByClassName('email-input')[0].value;
+    var selectOption = document.getElementsByClassName('select-input')[0].value;
+    
+    if (name === '' || email === '' || selectOption === '') {
+        alert("Tidak boleh ada kolom yang kosong!");
+        document.getElementById('warning-message').style.display = 'block';
+        return false;
+    }
+    
+    console.log('Nama:', name);
+    console.log('Email:', email);
+    console.log('Pilihan:', selectOption);
+    
+    return false;
+    
 }
 
-replaceName();
